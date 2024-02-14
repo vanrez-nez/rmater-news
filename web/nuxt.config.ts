a49c92e5949eb87ab190b0a5bdfde139711bb8c7
Ivan Juarez <ivan@madebyraygun.com>
2024-02-14 00:04:32 -0600

    feat: custom font loading

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: [
+    "~/assets/css/theme.css",
+    "~/assets/css/main.css"
+  ],
+  noscript: [{ innerHTML: "This website requires JavaScript." }],
+  modules: [
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    prefetch: true,
+    preconnect: true,
+    preload: true,
+    download: true,
+    outputDir: 'assets/fonts',
+    display: 'swap',
+    families: {
+      'Inter': {
+        wght: '100..900'
+      },
+    }
+  }
 })
