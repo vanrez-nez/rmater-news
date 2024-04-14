aaf3f6ba38451aa9a8ce2d6a2d700d9651db3ce7
Ivan Juarez <ivan@madebyraygun.com>
2024-04-13 22:09:14 -0600

    refact: extracts logic into ArticleCard component and api event handler

---
+++ web/components/ArticleCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: {
+    title: String,
+    date: String,
+    content: String
+  }
+}
+</script>
+
+<template>
+  <article>
+    <h2>{{ title }}</h2>
+    <time datetime="{{date}}">
+      <small>{{ date }}</small>
+    </time>
+    <p>{{ content }}</p>
+  </article>
+</template>
---
+++ web/server/api/today-news.ts
@@ -0,0 +1,19 @@
+import sqlite3 from "sqlite3";
+
+export default defineEventHandler(async (event) => {
+  // open sqlite db from file system at storage/news.db
+  const db = new sqlite3.Database('storage/news.db');
+
+  async function getNews() {
+    return new Promise((resolve, reject) => {
+      db.all('SELECT * FROM entries WHERE date > date("now", "-1 day") ORDER BY date DESC', (err:any, rows:any[]) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(rows);
+      });
+    });
+  }
+  // select all rows from news table
+  return await getNews();
+})
