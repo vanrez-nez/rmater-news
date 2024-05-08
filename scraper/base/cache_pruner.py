573d8ba4c741a5dc4ede03bf2338cd75858e2d16
Ivan Juarez <ivan@madebyraygun.com>
2024-05-07 23:21:48 -0600

    fix: increase cache pruning time

--- scraper/base/cache_pruner.py
+++ scraper/base/cache_pruner.py
@@ -4,7 +4,7 @@ from base.logger import log
 
 CACHE_DIR = "cache"
 
-def prune_cache_files(time_since_creation=3600*48) -> None:
+def prune_cache_files(time_since_creation=3600*24*30) -> None:
   log(f'Pruning cache files older than {time_since_creation/3600} hours')
   # scan cache directory for files
   for file in os.listdir(CACHE_DIR):
