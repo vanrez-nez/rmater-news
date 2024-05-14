abcc373390ea2170e71d9b92e55ed5da131fe128
Ivan Juarez <ivan@madebyraygun.com>
2024-05-14 00:31:48 -0600

    feat: langchain kind of working

--- scraper/transform/geo_tagging.py
+++ scraper/transform/geo_tagging.py
@@ -6,20 +6,84 @@ from base.logger import log
 from base.async_utils import async_wrapper
 from database.db_handler import get_articles_without_location
 from transform.prompts.geo_location import build_prompt
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_community.document_loaders import TextLoader
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_community.vectorstores import Chroma
+from langchain.prompts import PromptTemplate
+from langchain_community.llms import Ollama
+
+cached_llm = None
+glossary_contents = None
+folder_path = 'storage/db'
+embeddings = FastEmbedEmbeddings()
+text_splitter = RecursiveCharacterTextSplitter(
+  chunk_size=1024, chunk_overlap=80, length_function=len, is_separator_regex=False
+)
+
+raw_prompt = PromptTemplate.from_template(
+    """
+    <s>[INST] Eres un asistente de ubicaciones para Mexico. Analiza los mensajes e intenta extraer su ubicacion.[/INST] </s>
+    [INST] {input}
+           Context: {context}
+           Answer:
+    [/INST]
+"""
+)
+
+def load_llm():
+  global cached_llm
+  if (cached_llm is None):
+    host = os.environ.get("OLLAMA_HOST")
+    cached_llm = Ollama(base_url=host, model='llama3')
+
+@async_wrapper
+def load_glossary():
+  global glossary_contents
+  if (glossary_contents is None):
+    file = 'storage/glossary.txt'
+    print(f'Loading glossary from {file}')
+    loader = TextLoader(file, 'utf-8')
+    docs = loader.load_and_split()
+    print(f'Loaded {len(docs)} documents')
+    chunks = text_splitter.split_documents(docs)
+    print(f'Split into {len(chunks)} chunks')
+    vector_store = Chroma.from_documents(
+      documents=chunks,
+      embedding=embeddings,
+      persist_directory=folder_path
+    )
+    glossary_contents = True
 
 def get_articles() -> list[ScraperArticleType]:
-  return get_articles_without_location(10)
+  return get_articles_without_location(1)
 
-async def query_ollama(article: ScraperArticleType) -> dict:
-  host = os.environ.get("OLLAMA_HOST")
-  client = AsyncClient(host=host)
+@async_wrapper
+def query_ollama(article: ScraperArticleType) -> dict:
+  # host = os.environ.get("OLLAMA_HOST")
+  # client = AsyncClient(host=host)
+  load_llm()
+  # await load_glossary()
+  print('glosary loaded')
   prompt = build_prompt(article.title, article.content)
-  response = await client.chat(model='llama3', format='json', messages=[
-    {
-      'role': 'user',
-      'content': prompt,
+  vector_store = Chroma(persist_directory=folder_path, embedding_function=embeddings)
+  retriever = vector_store.as_retriever(
+    search_type="similarity_score_threshold",
+    search_kwargs={
+      "k": 20,
+      "score_threshold": 0.1,
     },
-  ])
+  )
+
+  document_chain = create_stuff_documents_chain(cached_llm, raw_prompt)
+  chain = create_retrieval_chain(retriever, document_chain)
+  result = chain.invoke({
+    "input": prompt
+  })
+  response = result['answer']
+  print(result)
   return response
 
 async def run():
@@ -27,10 +91,10 @@ async def run():
   for article in articles:
     response = await query_ollama(article)
     try:
-      content = response.get('message', {}).get('content', {})
+      content = response #.get('message', {}).get('content', {})
       json_response = json.loads(content)
       # validate response and required fields
-      fields = ['pais', 'estado', 'ciudad', 'municipio', 'colonia', 'calle', 'numero', 'lugar']
+      fields = ['pais', 'estado', 'ciudad', 'municipio', 'asentamiento', 'calle', 'numero', 'lugar']
       if not all(field in json_response for field in fields):
         log('Invalid response', json_response)
         continue
